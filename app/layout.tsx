e17f26e99a3368eb7234c0cb74fb3e2d5e06d216
nextezeizasur-design <nextezeizasur@gmail.com>
2026-05-28 22:50:43 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import "@/app/globals.css";
+import "./globals.css";
 import { Providers } from "@/components/shared/providers";
 import { Toaster } from "@/components/ui/toaster";
 
